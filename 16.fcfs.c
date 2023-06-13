14a3dc8f87d89e501c9522c6d9de123c8d75c0b7
Gsuueywoiehej <113223882+Gsuueywoiehej@users.noreply.github.com>
2023-06-13 08:41:04 +0530

    Add files via upload

---
+++ 16.fcfs.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int n=3,i;
+	int burst_time[]={10,15,25};
+	int waiting_time[n],turnaround_time[n];
+	waiting_time[0]=0;
+	for(i=1;i<n;i++){
+		waiting_time[i]=waiting_time[i-1]+burst_time[i-1];
+	}
+	for(i=0;i<n;i++)
+	{
+		turnaround_time[i]=waiting_time[i]+burst_time[i];
+	}
+	float avg_waiting_time=0,avg_turnaround_time=0;
+	for(i=0;i<n;i++){
+		avg_waiting_time+=waiting_time[i];
+		avg_turnaround_time+=turnaround_time[i];
+	}
+	avg_waiting_time/=n;
+	avg_turnaround_time/=n;
+	printf("process\t burst time\t waiting time\t turnaround time\n");
+	for(i=0;i<n;i++)
+	{
+		printf("p%d\t\t%d\t\t%d\n",i+1,burst_time[i],waiting_time[i],turnaround_time[i]);
+	}
+	printf("average waiting time:%.2f\n",avg_waiting_time);
+	printf("average turnaround time:%.2f\n",avg_turnaround_time);
+	return 0;
+}
